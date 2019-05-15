06150358062d60c5525c961c2324a6861d6ef494
Jake <jake.a.atkin@gmail.com>
2019-05-15 22:33:59 +0100

    Added functionality back that was lost

--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,33 +1,54 @@
 'use strict';
 
-//
-// function Powersave() {
-//   this._temperature =< 25
-// }
-
-
-function Thermostat() {
-  this._temperature = 20
+function Thermostat(){
+  this._temperature = 20;
+  this._minimumTemp = 10;
+  this._isPowerSaving = true;
+  this._resetTemperature = 20;
 }
 
-
-
-Thermostat.prototype = {
-  getCurrentTemperature: function() {
-    return this._temperature;
-  },
-  up: function() {
-      this.getCurrentTemperaturetemperate += 1;
-  },
-  down: function() {
-      this.getCurrentTemperature +-1
-  },
-  minimum: function() {
-    this.getCurrentTemperature >= 10
+Thermostat.prototype.maximumTemperature = function(){
+  if(this._isPowerSaving){
+    return 25;
   }
+  return 35;
+};
 
-}
+Thermostat.prototype.temperature = function(){
+  return this._temperature;
+};
 
+Thermostat.prototype.up = function(temp){
+  let newTemp = this._temperature + temp;
+
+  if(newTemp >= this.maximumTemperature()){
+    this._temperature = this.maximumTemperature();
+  }else{
+    this._temperature = newTemp;
+  }
+};
+
+Thermostat.prototype.down = function(temp){
+  if(this._temperature - temp < this._minimumTemp){
+    this._temperature = this._minimumTemp;
+  } else{
+  this._temperature -= temp;
+  }
+};
+
+Thermostat.prototype.reset = function() {
+  this._temperature = this._resetTemperature;
+};
+
+Thermostat.prototype.usage = function() {
+  if(this._temperature < 18) {
+    return 'low-usage'
+  } else if(this._temperature < 25) {
+    return 'medium-usage'
+  } else {
+    return 'high-usage'
+  }
+};
 
 
 
